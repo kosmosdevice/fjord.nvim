6e62875bc10474122cdbe9eea6abbe6022dadc86
Karl Walter <karl.walter@hotmail.com>
2025-10-06 19:11:45 +0200

    first setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ringo.nvim
 Ringo colorscheme for NeoVim
+
+## Credits
+- Ringo is forked from Ymir (https://github.com/Ronxvier/ymir.nvim)
+- Created using this guide: https://medium.com/@ronxvier/build-your-own-neovim-colorscheme-in-lua-3b01adf019e0
---
+++ lua/palette.lua
@@ -0,0 +1,57 @@
+return {
+
+  -- color approach
+  red = "#cc2c36",
+  orange = "#ec5f31",
+  green = "#21774d",
+  yellow = "#f4a934",
+  blue = "#203cb1",
+  magenta = "#c569b9",
+  cyan = "#298389",
+  black = "#0c0a0a",
+  bright_red = "#db5760",
+  bright_orange = "#f0815c",
+  bright_green = "#31b475",
+  bright_yellow = "#f6b855",
+  bright_blue = "#3d5ddb",
+  bright_magenta = "#d491cb",
+  bright_cyan = "#35a8b1",
+  white = "#d7c9c1",
+  gutter_fg = "#78808f",
+  nontext = "#d2d6dc",
+
+  -- element approach
+  bg         = "#292425",
+  fg         = "#51484a",
+  cursorline = "#1c2b3a",
+  selection  = "#2e3c55",
+  linenr     = "#4a5a70",
+  comment    = "#7d88a1",
+
+  -- Syntax
+  keyword    = "#f28fad",
+  Function   = "#89b4fa",
+  string     = "#f9e2af",
+  constant   = "#cdd6f4",
+  type       = "#cba6f7",
+  number     = "#fab387",
+  boolean    = "#f38ba8",
+  operator   = "#94a3b8",
+  variable   = "#e0def4",
+
+  -- UI
+  cursor     = "#f5a97f",
+  visual     = "#2e3c55",
+  search     = "#f5c2e7",
+  statusline = "#1e293b",
+  menu_bg    = "#1a2535",
+  menu_sel   = "#3b4252",
+  fold       = "#3e4a5a",
+  split      = "#334155",
+
+  -- Diagnostics
+  diag_error = "#f38ba8",
+  diag_warn  = "#fab387",
+  diag_info  = "#89dceb",
+  diag_hint  = "#b4befe",
+}
